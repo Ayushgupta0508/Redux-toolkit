d0cbaae653c40afb6952d09bb2cf5fbdf44dc414
Ayush Kumar <ayush030515@gmail.com>
2026-09-22 18:23:57 +0530

    Add navbar work

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
     <input type="text" onChange={(e)=>dispatch(changeName(e.target.value))} />
       <Counter/>
       <Users/>
+      <h1>Navbar work</h1>
       <h1>hello react</h1>
       <h2>hello ji </h2>
     </>
